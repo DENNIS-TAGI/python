f0319efb29233ef57287cd4117a8e6a10c2b4aad
DENNIS-TAGI <dennistagi2028@cs.sjcetpalai.ac.in>
2024-10-01 15:45:24 +0530

    Add files via upload

--- student_details.py
+++ student_details.py
@@ -1,6 +1,8 @@
-#"""Author:Dennis
-#Date:1/10/24
-#PYTHON PROGRAM TO GET STUDENT DETAILS
+"""
+Author:Dennis
+Date:1/10/24
+PYTHON PROGRAM TO GET STUDENT DETAILS
+"""
 name=input("enter the student name :")
 roll_no=int(input("enter your roll number:"))
 roll_no=roll_no+1
