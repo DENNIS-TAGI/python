e4e965416c91c6d00d0f74bee550f65919caad95
DENNIS-TAGI <dennistagi2028@cs.sjcetpalai.ac.in>
2024-10-08 15:02:31 +0530

    Add files via upload

---
+++ Concatenate the two strings.py
@@ -0,0 +1,8 @@
+first_name="DENNIS"
+last_name="TAGI"
+full_name=(first_name+" "+last_name)
+print(full_name)
+length=len(full_name)
+print(length)
+extarcted_first_name=full_name[:length-4]
+print(extarcted_first_name)
