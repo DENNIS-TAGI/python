e9a424da0433a20dafca4e64b4defe5dd430759c
DENNIS-TAGI <dennistagi2028@cs.sjcetpalai.ac.in>
2024-10-08 15:02:03 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,20 @@
+'''''
+AUTHOR NAME:DENNIS TAGI
+DATE:08/10/24
+SIMPLE DESKTOP CALCULATOR USING PYTHON
+'''
+num1=int(input("enter a number: "))
+num2=int(input("enter a number: "))
+num3=int(input("enter a number: "))
+sum=num1+num2
+print("the sum of num1 and num2:",sum)
+difference=num1-num2
+print("the difference when num2 is substracted from num1 is:",difference)
+multiply=num1*num2
+print("the product of num1 and num2 is:",multiply)
+divide=num1/num2
+print("the quotient when num1 is divided by num2 is:",divide)
+modulus=num1%num2
+print("the remainder when num1 is divided by num2 is:",modulus)
+result=(num1+num2)*num3/2
+print("the result of (num1+num2)*num3/2 is:",result)
