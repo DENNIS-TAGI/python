82935a6aca1f3171b7a16a4faa47b3eb6302afaa
DENNIS-TAGI <dennistagi2028@cs.sjcetpalai.ac.in>
2024-10-15 15:56:20 +0530

    Add files via upload

---
+++ odd.py
@@ -0,0 +1,11 @@
+'''
+python program to get odd numbers
+'''
+
+limit=int(input("enter the limit"))
+odd_number=1
+count=0
+while count<limit:
+     print(odd_number,"\t",end="")
+     count+=1
+     odd_number+=2
