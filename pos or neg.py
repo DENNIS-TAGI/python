397a32740a392e7c26b77bc030804f718ea4af86
DENNIS-TAGI <dennistagi2028@cs.sjcetpalai.ac.in>
2024-10-15 14:06:05 +0530

    Add files via upload

---
+++ pos or neg.py
@@ -0,0 +1,9 @@
+''''
+python program to check whether a number is positive or negative
+'''
+
+number=int(input("enter a number"))
+if number>0:
+     print("the given number:",number,"is positive")
+else:
+    print("the given number",number,"is negative")
